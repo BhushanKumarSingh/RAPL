a1dc34f4c4611b0f10fdb19f711fe66c3eb5ab41
BhushanKumarSingh <46242228+BhushanKumarSingh@users.noreply.github.com>
2020-12-21 23:18:05 +0530

    start

--- API/weighbridge.php
+++ API/weighbridge.php
@@ -1,13 +1,43 @@
 <?php
-include './config/db.php';
+//include Configurations
+include '../config/db.php';
+
+//declare requred parameters & methods
 $_PARAMS = array(
-    'name' => ['id'],
-    'type' => ['alphanumeric'],
-    'maxLength' => ['8'],
-  );
-  $_METHOD = 'GET';
-  echo json_encode(init_api($_METHOD, $_DATA, $_PARAMS, $_ERROR = ''));
-  
-  function execute_main(){
-    echo 'hello bhuahn';
-  }
+  'name' => ['slNo', 'gatePassNo','vehicleNo',
+            'purchaseHub','purchaseType','grossWt',
+            'tareWt','wastage','netWt','imgUrl'],
+  'type' => ['alphanumeric', 'alphanumeric','alphanumeric',
+             'alphanumeric', 'alphanumeric','alphanumeric', 'alphanumeric',
+              'alphanumeric','alphanumeric', 'alphanumeric'],
+  'maxLength' => ['8', '13', '255','255','255','255','255','255','255','255'],
+);
+$_METHOD = 'POST';
+
+// Call API, init API calls execute_main()
+echo json_encode(init_api($_METHOD, $_DATA, $_PARAMS, $_ERROR = ''));
+
+// API Main
+/*-----------main()---------*/
+function execute_main()
+{
+  global $_DATA, $con;
+  $slNo = $_DATA['slNo'];
+  $gatePassNo = $_DATA['gatePassNo'];
+  $vehicleNo = $_DATA['vehicleNo'];
+  $purchaseHub = $_DATA['purchaseHub'];
+  $purchaseType = $_DATA['purchaseType'];
+  $grossWt = $_DATA['grossWt'];
+  $tareWt = $_DATA['tareWt'];
+  $wastage = $_DATA['wastage'];
+  $netWt = $_DATA['netWt'];
+  $imgUrl = $_DATA['imgUrl'];
+
+    if($con->query("INSERT into weighbridge(slno)values('".$slNo."')")){
+        $returnArr = returnData('Save successfully', 200);
+    } else{
+        $returnArr = returnData('Something went wrong', 401);
+    }
+
+  return $returnArr;
+}
