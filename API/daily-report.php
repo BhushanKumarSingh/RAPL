5b7b757614f0ba94f79378ea6cb3576cd7523f95
BhushanKumarSingh <46242228+BhushanKumarSingh@users.noreply.github.com>
2020-12-27 19:45:54 +0530

    Update daily-report.php

--- API/daily-report.php
+++ API/daily-report.php
@@ -12,7 +12,7 @@ include './config/db.php';
 function execute_main(){
   global $_DATA, $con;
     $date = $_DATA['date'];
-    $weighList = $con->query("SELECT slno As SL_NO , date as Date, vehicleno As Vehicle_No,purchasehub as PACS,acnote_no as AC_Note_No ,acnote_date as AC_Note_Date,acnote_bags as AC_Note_Bags,grosswt as Gross_Wt,tarewt as Tare_Wt, wastage as Wastage,netwt as Net_Wt ");
+    $weighList = $con->query("SELECT slno As SL_NO , date as Date, vehicleno As Vehicle_No,purchasehub as PACS,acnote_no as AC_Note_No ,acnote_date as AC_Note_Date,acnote_bags as AC_Note_Bags,grosswt as Gross_Wt,tarewt as Tare_Wt, wastage as Wastage,netwt as Net_Wt from weighbridge");
     $weigh = [];
     while($data = $weighList->fetch_assoc()){
       array_push($weigh,$data);
