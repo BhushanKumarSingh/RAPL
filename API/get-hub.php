07bf0d741824a878d6db0f09e5d65b1ad1404fad
BhushanKumarSingh <46242228+BhushanKumarSingh@users.noreply.github.com>
2020-12-27 23:46:27 +0530

    Update get-hub.php

--- API/get-hub.php
+++ API/get-hub.php
@@ -15,14 +15,18 @@ function execute_main()
   }
 
 
-  $hub = [];
+  $hub['purchaseHub'] = [];
+  $hub['purchaseTarget'] = [];
+
   while($data = $hubDetails->fetch_assoc()){
-    if(array_key_exists($data['purchase_hub'],$hub)){
-      array_push($hub[$data['purchase_hub']],$data['purchase_target']);
-    } else{
-      $hub[$data['purchase_hub']] = [];
-      array_push($hub[$data['purchase_hub']],$data['purchase_target']);
-    }
+    // if(array_key_exists($data['purchase_hub'],$hub)){
+    //   array_push($hub[$data['purchase_hub']],$data['purchase_target']);
+    // } else{
+    //   $hub[$data['purchase_hub']] = [];
+    //   array_push($hub[$data['purchase_hub']],$data['purchase_target']);
+    // }
+      array_push($hub['purchaseTarget'],$data['purchase_target']);
+      array_push($hub['purchaseHub'],$data['purchase_hub']);
   }
   echo json_encode(array("ResponseCode"=>200,"ResponseMsg"=>"Hub data","response"=>$hub));
 }
