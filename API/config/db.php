4634c9fd0da7549f21883ee99f0db9d491a9f240
BhushanKumarSingh <46242228+BhushanKumarSingh@users.noreply.github.com>
2020-12-30 00:10:13 +0530

    cros

--- API/config/db.php
+++ API/config/db.php
@@ -2,26 +2,6 @@
 if(!isset($_SESSION)) { session_start(); }
 
 
-// Allow from any origin
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    // should do a check here to match $_SERVER['HTTP_ORIGIN'] to a
-    // whitelist of safe domains
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');    // cache for 1 day
-}
-header("Access-Control-Allow-Origin: *");
-// Access-Control headers are received during OPTIONS requests
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-}
-
 //set timezone
 date_default_timezone_set('Asia/Kolkata');
 setlocale(LC_MONETARY, 'en_US');
