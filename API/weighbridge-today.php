7face06aa2adc92e96c028e960de4d1692a2ad28
BhushanKumarSingh <46242228+BhushanKumarSingh@users.noreply.github.com>
2020-12-30 01:48:04 +0530

    Update weighbridge-today.php

--- API/weighbridge-today.php
+++ API/weighbridge-today.php
@@ -36,7 +36,7 @@ function execute_main()
   $where = "WHERE (date BETWEEN '".$fromdate."' and '".$todate."')";
 
   $date = $_DATA['date'];
-  $weighList = $con->query("SELECT slno As SL_NO , date as Date, vehicleno As Vehicle_No,purchasehub as PACS,acnote_no as AC_Note_No ,acnote_date as AC_Note_Date,acnote_bags as AC_Note_Bags,grosswt as Gross_Wt,tarewt as Tare_Wt, wastage as Wastage,netwt as Net_Wt  from weighbridge ".$where."");
+  $weighList = $con->query("SELECT *  from weighbridge ".$where."");
   $weigh = [];
   while($data = $weighList->fetch_assoc()){
     array_push($weigh,$data);
